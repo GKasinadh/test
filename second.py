a84527983975bff7999feb7c68a91ffd469563a1
GKasinadh <gunta.kasinadh@lumiq.ai>
2023-01-21 19:23:34 +0530

    added reversing a number

--- second.py
+++ second.py
@@ -5,11 +5,19 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+def reverse_number(num):
+    return int(str(num)[::-1])
+
 num = int(input("Enter a number :"))
 
 if is_prime(num) == True:
     print(num, "is a prime number")
+    print(reverse_number(num))
 else:
     print(num, "is not a prime number")
+
+
+
+
 
 
